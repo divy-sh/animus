9b4e62fb04b54cd1b979f2a77a5357918f1000bb
divyendu <divyendua@gmail.com>
2025-03-09 00:25:13 -0500

    add listCommands test

--- commands/listCommands_test.go
+++ commands/listCommands_test.go
@@ -16,6 +16,15 @@ func TestRPush(t *testing.T) {
 	}
 }
 
+func TestRPushInvalidArgs(t *testing.T) {
+	args := []resp.Value{}
+	result := rpush(args)
+	expected := "ERR wrong number of arguments for 'rpush' command"
+	if result.Typ != "error" || result.Str != expected {
+		t.Errorf("Expected error %s, got %v", expected, result)
+	}
+}
+
 func TestRPopEmpty(t *testing.T) {
 	listKey := "emptylist"
 	args := []resp.Value{{Typ: "bulk", Bulk: listKey}}
@@ -38,9 +47,22 @@ func TestRPopSingle(t *testing.T) {
 	}
 }
 
+func TestRPopInvalidArgs(t *testing.T) {
+	args := []resp.Value{}
+	result := rpop(args)
+	expected := "ERR wrong number of arguments for 'rpop' command"
+	if result.Typ != "error" || result.Str != expected {
+		t.Errorf("Expected error %s, got %v", expected, result)
+	}
+}
+
 func TestRPopMultiple(t *testing.T) {
 	listKey := "multilist"
-	rpush([]resp.Value{{Typ: "bulk", Bulk: listKey}, {Typ: "bulk", Bulk: "val1"}, {Typ: "bulk", Bulk: "val2"}, {Typ: "bulk", Bulk: "val3"}})
+	rpush([]resp.Value{
+		{Typ: "bulk", Bulk: listKey},
+		{Typ: "bulk", Bulk: "val1"},
+		{Typ: "bulk", Bulk: "val2"},
+		{Typ: "bulk", Bulk: "val3"}})
 	args := []resp.Value{{Typ: "bulk", Bulk: listKey}, {Typ: "bulk", Bulk: "2"}}
 	result := rpop(args)
 
