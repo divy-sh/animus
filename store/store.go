d810b7fa5110ea92f3962a7531be636da4d753b5
divyendu <divyendua@gmail.com>
2025-04-17 05:39:35 -0400

    move store cleaner subprocess into its own file

--- store/store.go
+++ store/store.go
@@ -1,8 +1,6 @@
 package store
 
-// TODO fix the auto deletion algorithm
 import (
-	"math/rand"
 	"sync"
 	"time"
 
@@ -86,99 +84,4 @@ func Delete[K comparable](key K) {
 	store.mutex.Lock()
 	defer store.mutex.Unlock()
 	store.LRUCache.Remove(key)
-}
-
-func StartExpiryCleaner() {
-	store.mutex.Lock()
-	defer store.mutex.Unlock()
-	if store.isRunning {
-		return
-	}
-	store.isRunning = true
-	store.stopCleaner = make(chan struct{})
-	go expiryCleanerLoop(store)
-}
-
-func StopExpiryCleaner() {
-	store.mutex.Lock()
-	defer store.mutex.Unlock()
-	if !store.isRunning {
-		return
-	}
-	store.isRunning = false
-	close(store.stopCleaner)
-}
-
-func expiryCleanerLoop(store *Store) {
-	ticker := time.NewTicker(100 * time.Millisecond)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
-			cleanExpiredKeys()
-		case <-store.stopCleaner:
-			return
-		}
-	}
-}
-
-func cleanExpiredKeys() {
-	const (
-		sampleSize    = 20
-		targetPercent = 25.0
-		maxIterations = 3
-	)
-	now := time.Now().Unix()
-	store.mutex.RLock()
-	allKeys := store.LRUCache.Keys()
-	store.mutex.RUnlock()
-	if len(allKeys) == 0 {
-		return
-	}
-	for iteration := 0; iteration < maxIterations; iteration++ {
-		keysToCheck := sampleRandomKeys(allKeys, sampleSize)
-		if len(keysToCheck) == 0 {
-			return
-		}
-		// Count expired keys and delete them
-		expiredCount := 0
-		for _, key := range keysToCheck {
-			store.mutex.RLock()
-			val, ok := store.LRUCache.Get(key)
-			store.mutex.RUnlock()
-			if !ok {
-				continue
-			}
-			value := val.(*Value)
-			if value.TTL > -1 && value.TTL < now {
-				expiredCount++
-				store.mutex.Lock()
-				store.LRUCache.Remove(key)
-				store.mutex.Unlock()
-			}
-		}
-		if len(keysToCheck) == 0 {
-			return
-		}
-		expiryPercentage := float64(expiredCount) / float64(len(keysToCheck)) * 100
-		if expiryPercentage < targetPercent {
-			return
-		}
-	}
-}
-
-func sampleRandomKeys(keys []any, sampleSize int) []any {
-	if len(keys) == 0 {
-		return nil
-	}
-	if len(keys) <= sampleSize {
-		return keys
-	}
-	shuffled := make([]any, len(keys))
-	copy(shuffled, keys)
-	for i := range shuffled {
-		j := rand.Intn(i + 1)
-		shuffled[i], shuffled[j] = shuffled[j], shuffled[i]
-	}
-	return shuffled[:sampleSize]
 }
