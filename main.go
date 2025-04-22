2d248a95b1d696512c3e00011c5fcea9d0992722
divyendu <divyendua@gmail.com>
2025-04-22 02:43:45 -0400

    add support for concurrent clients

--- main.go
+++ main.go
@@ -1,65 +1,9 @@
 package main
 
 import (
-	"log"
-	"net"
-	"strings"
-
-	"github.com/divy-sh/animus/internal/command"
-	"github.com/divy-sh/animus/internal/common"
-	"github.com/divy-sh/animus/internal/resp"
+	"github.com/divy-sh/animus/cmd/animus"
 )
 
 func main() {
-	log.Print("Listening on port :6379")
-
-	// Create a new server
-	l, err := net.Listen("tcp", ":6379")
-	if err != nil {
-		log.Print(err)
-		return
-	}
-
-	// Listen for connections
-	conn, err := l.Accept()
-	if err != nil {
-		log.Print(err)
-		return
-	}
-
-	defer conn.Close()
-
-	for {
-		reader := resp.NewReader(conn)
-		value, err := reader.Read()
-		if err != nil {
-			log.Print(err)
-			return
-		}
-
-		if value.Typ != "array" {
-			log.Print("Invalid request, expected array")
-			continue
-		}
-
-		if len(value.Array) == 0 {
-			log.Print("Invalid request, expected array length > 0")
-			continue
-		}
-
-		cmd := strings.ToUpper(value.Array[0].Bulk)
-		args := value.Array[1:]
-
-		writer := resp.NewWriter(conn)
-
-		handler, ok := command.Handlers[cmd]
-		if !ok {
-			log.Print("Invalid command: ", cmd)
-			writer.Write(resp.Value{Typ: common.STRING_TYPE, Str: ""})
-			continue
-		}
-
-		result := handler.Func(args)
-		writer.Write(result)
-	}
+	animus.Handle()
 }
